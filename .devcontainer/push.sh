d4d8c505a8203deb0a73c6c18343e6f60e565c80
Ohkubo KOHEI <o@kbn.one>
2026-04-07 14:02:23 +0900

    update ci

--- .devcontainer/push.sh
+++ .devcontainer/push.sh
@@ -3,8 +3,9 @@
 # get classic token
 # https://github.com/settings/tokens/new?scopes=write:packages,delete:packages&description=ghcr
 USER=kuboon
-NAME=wast-dev-container
-REPO=ghcr.io/${USER}/${NAME}
+REPO=wast
+IMAGE=wast-dev-container
+FULL=ghcr.io/${USER}/${REPO}/${IMAGE}
+TAG=latest
 # yes | docker login ghcr.io -u ${USER} || docker login ghcr.io -u ${USER}
-docker buildx build --platform linux/amd64,linux/arm64 --push -t ${REPO}:latest .
-docker push ${REPO}:latest
+docker buildx build --platform linux/amd64,linux/arm64 --push -t ${FULL}:latest .
